0a9612159581df47cdf6ce498cd8d2de792f93b7
stephane.cachat.ext@aprr.fr <scachat@sstinformatique.fr>
2025-04-11 11:01:18 +0200

    déplacement de synoptique dans son propre projet

--- src/main/java/com/cachat/prj/echo3/synoptique/ClicListener.java
+++
@@ -1,9 +0,0 @@
-package com.cachat.prj.echo3.synoptique;
-
-/**
- * indique qu'un objet a été cliqué
- * @author scachat
- */
-public interface ClicListener {
-    public void clic(SynObject source);    
-}
