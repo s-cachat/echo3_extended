f906fab00e0fe4279ffe7d44d8bf870ee5f0f226
Stephane Cachat <stephane@cachat.com>
2026-06-28 22:54:35 +0200

    avancement

--- src/main/resources/com/cachat/prj/echo3/components/js/Gauge.js
+++ src/main/resources/com/cachat/prj/echo3/components/js/Gauge.js
@@ -16,11 +16,33 @@ Gauge.Sync = Core.extend(Echo.Render.ComponentSync, {
         console.log(update);
         this._field = document.createElement("canvas");
         this._field.id = this.component.renderId;
+        var opts = {
+            angle: -0.2, // The span of the gauge arc
+            lineWidth: 0.2, // The line thickness
+            radiusScale: 1, // Relative radius
+            pointer: {
+                length: 0.4, // // Relative to gauge radius
+                strokeWidth: 0.044, // The thickness
+                color: '#000000' // Fill color
+            },
+            colorStart: '#6FADCF',
+            colorStop: '#8FC0DA',
+            strokeColor: '#E0E0E0',
+            generateGradient: true,
+            highDpiSupport: true, // High resolution support
 
-        this.gauge = new Gauge(this._field).setOptions(opts);
-        this.gauge.maxValue = this.component.render("maxValue");
-        this.gauge.setMinValue(this.component.render("minValue"));
-        this.gauge.set(this.component.render("value"));
+        };
+        this.gauge = new Gauge(this._field)
+        try {
+            console.log(this.gauge);
+            this.gauge.setOptions(opts);
+            this.gauge.maxValue = this.component.render("maxValue");
+            this.gauge.minValue = this.component.render("minValue");
+            this.gauge.set(this.component.render("value"));
+        } catch (e) {
+            console.log(e);
+        }
+
         const _this = this;
         Echo.Sync.renderComponentDefaults(this.component, this._field);
         parentElement.appendChild(this._field);
